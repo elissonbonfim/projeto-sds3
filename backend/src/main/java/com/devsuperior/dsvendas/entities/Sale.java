574f0b788d71c0002421a6a0c9cb5c39f80550ab
elisson <elisson100@outlook.com>
2021-05-05 21:25:45 -0300

    Data Base seed

--- backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -6,19 +6,32 @@
 package com.devsuperior.dsvendas.entities;
 
 import java.time.LocalDate;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 /**
  *
  * @author elisson
  */
+@Entity
+@Table(name = "tb_sales")
 public class Sale {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Integer visited;
     private Integer deals;
     private Double amount;
-    private LocalDate data;
+    private LocalDate date;
     
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
     private Seller seller;
 
     public Sale() {
@@ -29,7 +42,7 @@ public class Sale {
         this.visited = visited;
         this.deals = deals;
         this.amount = amount;
-        this.data = data;
+        this.date = data;
         this.seller = seller;
     }
 
@@ -66,11 +79,11 @@ public class Sale {
     }
 
     public LocalDate getData() {
-        return data;
+        return date;
     }
 
     public void setData(LocalDate data) {
-        this.data = data;
+        this.date = data;
     }
 
     public Seller getSeller() {
--- backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -7,26 +7,27 @@ package com.devsuperior.dsvendas.entities;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
  *
  * @author elisson
  */
-//@Entity
-//@Table(name = "dt_sellers")
+@Entity
+@Table(name = "tb_sellers")
 public class Seller {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     
+    @OneToMany(mappedBy = "seller")
     private List<Sale> sales = new ArrayList<>();
 
     public Seller() {
